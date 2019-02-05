48e2a4bc99f297fceb2e5e7f3d48a0906703999e
leogtzr <leogutierrezramirez@gmail.com>
2019-02-05 10:25:52 -0700

    adding logic to handle /design requests

---
+++ taco-app/src/main/java/com/taco/web/DesignTacoController.java
@@ -0,0 +1,49 @@
+package com.taco.web;
+
+import com.taco.domain.Ingredient;
+import com.taco.domain.Ingredient.Type;
+import com.taco.domain.Taco;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    @GetMapping
+    public String showDesignForm(final Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+        );
+
+        final Type[] types = Type.values();
+        for (final Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+
+        model.addAttribute("design", new Taco());
+
+        return "design";            // the logical view "design.html"
+    }
+
+    private Ingredient filterByType(final List<Ingredient> ingredients, Type type) {
+        return ingredients.stream().filter(t -> t.equals(type)).findFirst().get();
+    }
+
+}

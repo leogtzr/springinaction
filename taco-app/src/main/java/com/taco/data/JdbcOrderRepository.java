fd226659557ddcf4fc3e030f18dfae4e30fa614b
Leo <leogutierrezramirez@gmail.com>
2019-02-15 22:10:38 -0700

    removing unnecesssary comments

--- taco-app/src/main/java/com/taco/data/JdbcOrderRepository.java
+++ taco-app/src/main/java/com/taco/data/JdbcOrderRepository.java
@@ -33,9 +33,7 @@ public class JdbcOrderRepository implements OrderRepository {
 
     this.objectMapper = new ObjectMapper();
   }
-// end::core[]
 
-// tag::save[]
   @Override
   public Order save(Order order) {
     order.setPlacedAt(new Date());
@@ -68,16 +66,4 @@ public class JdbcOrderRepository implements OrderRepository {
     values.put("taco", taco.getId());
     orderTacoInserter.execute(values);
   }
-// end::save[]
-
-/*
-// tag::core[]
-
-...
-
-// end::core[]
- */
-
-// tag::core[]
 }
-// end::core[]

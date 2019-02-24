8c76a324cbc81ef4d462525b740b6eb9a6fefee5
Leo <leogutierrezramirez@gmail.com>
2019-02-23 18:40:17 -0700

    removing deprecated password setting code

--- taco-app-jpa/src/main/java/com/taco/config/SecurityConfig.java
+++ taco-app-jpa/src/main/java/com/taco/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -11,10 +13,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final AuthenticationManagerBuilder authBuilder) throws Exception {
+
+        final PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
         authBuilder
                 .inMemoryAuthentication()
                 .withUser("leo")
-                .password("{noop}lein23")
+                .password(encoder.encode("lein23"))
+                //.password("{noop}lein23")
                 .authorities("ROLE_USER")
                 .and()
                 .withUser("woody")

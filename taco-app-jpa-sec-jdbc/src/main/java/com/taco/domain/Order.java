a6094d8806ae4bae87d845515336063329d11073
leogtzr <leogutierrezramirez@gmail.com>
2019-02-26 09:06:57 -0700

    knowing the user

--- taco-app-jpa-sec-jdbc/src/main/java/com/taco/domain/Order.java
+++ taco-app-jpa-sec-jdbc/src/main/java/com/taco/domain/Order.java
@@ -19,49 +19,52 @@ import lombok.Data;
 @Table(name = "Taco_Order")
 public class Order implements Serializable {
 
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  @Id
-  private Long id;
-  
-  private Date placedAt;
-  
-  @NotBlank(message="Delivery name is required")
-  private String deliveryName;
-  
-  @NotBlank(message="Street is required")
-  private String deliveryStreet;
-  
-  @NotBlank(message="City is required")
-  private String deliveryCity;
-  
-  @NotBlank(message="State is required")
-  private String deliveryState;
-  
-  @NotBlank(message="Zip code is required")
-  private String deliveryZip;
+    @Id
+    private Long id;
 
-  @CreditCardNumber(message="Not a valid credit card number")
-  private String ccNumber;
-  
-  @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
-           message="Must be formatted MM/YY")
-  private String ccExpiration;
+    private Date placedAt;
 
-  @Digits(integer=3, fraction=0, message="Invalid CVV")
-  private String ccCVV;
+    @NotBlank(message="Delivery name is required")
+    private String deliveryName;
 
-  @ManyToMany(targetEntity = Taco.class)
-  private List<Taco> tacos = new ArrayList<>();
-  
-  public void addDesign(final Taco design) {
-    this.tacos.add(design);
-  }
+    @NotBlank(message="Street is required")
+    private String deliveryStreet;
 
-  @PrePersist
-  void placedAt() {
-    this.placedAt = new Date();
-  }
-  
+    @NotBlank(message="City is required")
+    private String deliveryCity;
+
+    @NotBlank(message="State is required")
+    private String deliveryState;
+
+    @NotBlank(message="Zip code is required")
+    private String deliveryZip;
+
+    @CreditCardNumber(message="Not a valid credit card number")
+    private String ccNumber;
+
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message="Must be formatted MM/YY")
+    private String ccExpiration;
+
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
+    private String ccCVV;
+
+    @ManyToMany(targetEntity = Taco.class)
+    private List<Taco> tacos = new ArrayList<>();
+
+    @ManyToOne
+    private User user;
+
+    public void addDesign(final Taco design) {
+        this.tacos.add(design);
+    }
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
+    }
+
 }
 
--- taco-app-jpa-sec-jdbc/src/main/java/com/taco/web/OrderController.java
+++ taco-app-jpa-sec-jdbc/src/main/java/com/taco/web/OrderController.java
@@ -2,6 +2,9 @@ package com.taco.web;
 
 import javax.validation.Valid;
 
+import com.taco.domain.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,29 +20,38 @@ import com.taco.data.OrderRepository;
 @RequestMapping("/orders")
 @SessionAttributes("order")
 public class OrderController {
-  
-  private OrderRepository orderRepo;
 
-  public OrderController(OrderRepository orderRepo) {
-    this.orderRepo = orderRepo;
-  }
-  
-  @GetMapping("/current")
-  public String orderForm() {
-    return "orderForm";
-  }
+    private OrderRepository orderRepo;
 
-  @PostMapping
-  public String processOrder(@Valid Order order, Errors errors, 
-                             SessionStatus sessionStatus) {
-    if (errors.hasErrors()) {
-      return "orderForm";
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
     }
-    
-    orderRepo.save(order);
-    sessionStatus.setComplete();
-    
-    return "redirect:/";
-  }
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(
+            @Valid Order order,
+            final Errors errors,
+            final SessionStatus sessionStatus,
+            // final Authentication authentication
+            final @AuthenticationPrincipal User user
+    ) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        // final User user = (User) authentication.getPrincipal();
+        order.setUser(user);
+
+        orderRepo.save(order);
+
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
 
 }

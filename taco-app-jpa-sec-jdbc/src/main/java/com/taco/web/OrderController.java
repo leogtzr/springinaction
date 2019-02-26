c845f8e06406955119ce5538223c4acb76548a25
leogtzr <leogutierrezramirez@gmail.com>
2019-02-26 09:11:02 -0700

    more code to know the user

--- taco-app-jpa-sec-jdbc/src/main/java/com/taco/web/OrderController.java
+++ taco-app-jpa-sec-jdbc/src/main/java/com/taco/web/OrderController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import com.taco.domain.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,6 +44,10 @@ public class OrderController {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+
+
+        // final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // final User user = (User) authentication.getPrincipal();
 
         // final User user = (User) authentication.getPrincipal();
         order.setUser(user);

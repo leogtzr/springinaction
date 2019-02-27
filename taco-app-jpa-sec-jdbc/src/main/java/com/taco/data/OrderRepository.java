f299963cadeb217783ffc5decc3703a877fe6787
Leo <leogutierrezramirez@gmail.com>
2019-02-26 20:50:25 -0700

    defining pagesize

--- taco-app-jpa-sec-jdbc/src/main/java/com/taco/data/OrderRepository.java
+++ taco-app-jpa-sec-jdbc/src/main/java/com/taco/data/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.taco.data;
 
 import com.taco.domain.Order;
+import com.taco.domain.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Date;
@@ -11,4 +13,6 @@ public interface OrderRepository extends CrudRepository<Order, Long> {
     List<Order> findByDeliveryZip(String deliveryZip);
     List<Order> readOdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
 
+    List<Order> findByUserOrderByPlacedAtDesc(User user);
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
--- taco-app-jpa-sec-jdbc/src/main/java/com/taco/web/OrderController.java
+++ taco-app-jpa-sec-jdbc/src/main/java/com/taco/web/OrderController.java
@@ -3,10 +3,14 @@ package com.taco.web;
 import javax.validation.Valid;
 
 import com.taco.domain.User;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,9 +24,14 @@ import com.taco.data.OrderRepository;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
+@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
 
     private OrderRepository orderRepo;
+    // the following property is set by using:
+    // taco.orders.pageSize = XXX
+    // or: export TACO_ORDERS_PAGESIZE=10
+    private int pageSize;
 
     public OrderController(OrderRepository orderRepo) {
         this.orderRepo = orderRepo;
@@ -59,4 +68,19 @@ public class OrderController {
         return "redirect:/";
     }
 
+    @GetMapping
+    public String ordersForUser(
+            final @AuthenticationPrincipal User user,
+            final Model model
+    ) {
+
+        final Pageable pageable = PageRequest.of(0, pageSize);
+        model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+
+        return "orderList";
+    }
+
+    public void setPageSize(final int pageSize) {
+        this.pageSize = pageSize;
+    }
 }

9454487671c2178cbcd825129d90f8a5afa88dde
Leo <leogutierrezramirez@gmail.com>
2019-03-07 10:36:00 -0700

    changing book's rel resource path

--- data-rest-repo/src/main/java/com/rest/repo/domain/Book.java
+++ data-rest-repo/src/main/java/com/rest/repo/domain/Book.java
@@ -2,6 +2,7 @@ package com.rest.repo.domain;
 
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,10 +10,9 @@ import javax.persistence.Id;
 @Data
 @Entity
 @ToString
+@RestResource(rel = "books", path = "libros")
 public class Book {
-
     @Id
     private long id;
     private String title;
-
 }

c3bfcc59c17238a8a2137e30a5c4e41e969bbc55
leogtzr <leogutierrezramirez@gmail.com>
2019-03-07 10:54:54 -0700

    applying spring data rest's base path to a controller

---
+++ taco-app-jpa-sec-jdbc/src/main/java/com/taco/web/api/RecentTacosController.java
@@ -0,0 +1,49 @@
+package com.taco.web.api;
+
+import com.taco.data.TacoRepository;
+import com.taco.domain.Taco;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+import java.util.Collection;
+import java.util.List;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@RepositoryRestController
+public class RecentTacosController {
+
+    private TacoRepository tacoRepository;
+
+    public RecentTacosController(final TacoRepository tacoRepository) {
+        this.tacoRepository = tacoRepository;
+    }
+
+    @GetMapping(path = "/tacos/recent", produces = "application/hal+json")
+    public ResponseEntity<Resources<TacoResource>> recentTacos() {
+        final PageRequest page = PageRequest.of(
+                0, 12, Sort.by("createdAt").descending());
+        final Collection<Taco> tacos = tacoRepository.findAllPage(page).getContent();
+
+        List<TacoResource> tacoResources =
+                new TacoResourceAssembler().toResources(tacos);
+        Resources<TacoResource> recentResources =
+                new Resources<TacoResource>(tacoResources);
+        recentResources.add(
+                linkTo(methodOn(RecentTacosController.class).recentTacos())
+                        .withRel("recents"));
+        return new ResponseEntity<>(recentResources, HttpStatus.OK);
+    }
+
+
+}

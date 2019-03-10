beafa0982bf1b68e7115b28d71207a16fe7b85b4
Leo <leogutierrezramirez@gmail.com>
2019-03-09 19:59:05 -0700

    fixing comp issue

---
+++ books-rest-repo/src/main/java/com/books/BooksRestRepoApplication.java
@@ -0,0 +1,11 @@
+package com.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksRestRepoApplication {
+	public static void main(final String[] args) {
+		SpringApplication.run(BooksRestRepoApplication.class, args);
+	}
+}

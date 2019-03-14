1a8c2bdcde14c46c21270308dc12f3532fb03981
Leo <leogutierrezramirez@gmail.com>
2019-03-13 20:04:45 -0700

    adding small project to play with jms

---
+++ jms-playing/src/main/java/com/jms/JmsPlayingApplication.java
@@ -0,0 +1,11 @@
+package com.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsPlayingApplication {
+	public static void main(final String[] args) {
+		SpringApplication.run(JmsPlayingApplication.class, args);
+	}
+}

cac87da16966a67e86faba02014315a7b4eeeb11
leogtzr <leogutierrezramirez@gmail.com>
2019-04-26 11:14:49 -0600

    add kafka listener

--- jms-playing2/src/main/java/com/jms/messaging/kafka/OrderListener.java
+++ jms-playing2/src/main/java/com/jms/messaging/kafka/OrderListener.java
@@ -2,11 +2,16 @@ package com.jms.messaging.kafka;
 
 import com.jms.domain.KitchenUI;
 import com.jms.domain.Order;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class OrderListener {
 
     private KitchenUI ui;
@@ -17,8 +22,19 @@ public class OrderListener {
     }
 
     @KafkaListener(topics = "tacocloud.orders.topic")
-    public void handle(final Order order) {
+//    public void handle(final Order order) {
+//        ui.displayOrder(order);
+//    }
+//    public void handle(final Order order, final ConsumerRecord<Order, Order> record) {
+//        log.info("Received from partition {} with timestam p {}", record.partition(), record.timestamp());
+//        ui.displayOrder(order);
+//    }
+    public void handle(final Order order, final Message<Order> message) {
+        final MessageHeaders headers = message.getHeaders();
+        log.info("Received from partition {} with timestamp {}", headers.get(KafkaHeaders.RECEIVED_PARTITION_ID), headers.get(KafkaHeaders.RECEIVED_TIMESTAMP));
+        //headers.get(KafkaHeaders.RECEIVED_TIMESTAMP)
         ui.displayOrder(order);
     }
+
 
 }

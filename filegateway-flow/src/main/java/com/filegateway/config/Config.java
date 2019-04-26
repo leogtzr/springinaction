c7d6f3d05345004e83c2c73d6b4e062ac20b56bd
leogtzr <leogutierrezramirez@gmail.com>
2019-04-26 13:11:09 -0600

    code to use spring integration with a simple rest endpoint

---
+++ filegateway-flow/src/main/java/com/filegateway/config/Config.java
@@ -0,0 +1,37 @@
+package com.filegateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.support.FileExistsMode;
+import org.springframework.integration.transformer.GenericTransformer;
+
+import java.io.File;
+
+@Configuration
+public class Config {
+
+    @Profile("javaconfig")
+    @Bean
+    @Transformer(inputChannel="textInChannel",
+            outputChannel="fileWriterChannel")
+    public GenericTransformer<String, String> upperCaseTransformer() {
+        return text -> text.toUpperCase();
+    }
+
+    @Profile("javaconfig")
+    @Bean
+    @ServiceActivator(inputChannel="fileWriterChannel")
+    public FileWritingMessageHandler fileWriter() {
+        FileWritingMessageHandler handler =
+                new FileWritingMessageHandler(new File("/tmp/sia5/files"));
+        handler.setExpectReply(false);
+        handler.setFileExistsMode(FileExistsMode.APPEND);
+        handler.setAppendNewLine(true);
+        return handler;
+    }
+
+}
---
+++ filegateway-flow/src/main/java/com/filegateway/config/FileWriterGateway.java
@@ -0,0 +1,13 @@
+package com.filegateway.config;
+
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.file.FileHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+
+@MessagingGateway(defaultRequestChannel = "textInChannel")
+public interface FileWriterGateway {
+    void writeToFile(
+            @Header(FileHeaders.FILENAME) String filename,
+            String data);
+
+}
